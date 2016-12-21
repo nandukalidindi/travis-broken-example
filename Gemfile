a60928ec9e1160da9dea6e3df0acd187104ac89c
nandukalidindi <nandukalidindi@gmail.com>
2016-12-21 02:41:20 -0500

    Add minitest framework

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'test-unit', '3.1.8'
+gem 'minitest', '5.8.4'
--- test.rb
+++ test.rb
@@ -1,11 +1,18 @@
-require 'test/unit'
-require 'test/unit/ui/console/testrunner'
+# require 'test/unit'
+# require 'test/unit/ui/console/testrunner'
+#
+#
+# class My_test < Test::Unit::TestCase
+#   def test_sample
+#     assert_equal(2, 2)
+#   end
+# end
+#
+# Test::Unit::UI::Console::TestRunner.run(My_test)
+require "minitest/autorun"
 
-
-class My_test < Test::Unit::TestCase
-  def test_sample
-    assert_equal(2, 2)
+class TestMeme < Minitest::Test
+  def test_random
+    assert_equal 1, 1
   end
 end
-
-Test::Unit::UI::Console::TestRunner.run(My_test)
